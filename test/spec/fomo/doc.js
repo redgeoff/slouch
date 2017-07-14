13a99359c3bd8ec6d0d13b28a5a6b05c7da3d5f2
Geoff Cox <redgeoff@gmail.com>
2017-07-13 22:31:40 -0700

    refactor(doc)

---
+++ test/spec/fomo/doc.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var Slouch = require('../../../scripts/fomo'),
+  utils = require('../../utils'),
+  sporks = require('sporks');
+
+describe('doc', function () {
+
+  var slouch = new Slouch(utils.couchDBURL()),
+    db = slouch.db,
+    doc = slouch.doc;
+
+  beforeEach(function () {
+    return db.create('testdb');
+  });
+
+  afterEach(function () {
+    return db.destroy('testdb');
+  });
+
+  it('should post, put and get doc', function () {
+    var doc = {
+      thing: 'play'
+    };
+
+    return slouch.doc.post('testdb', doc).then(function (_doc) {
+      doc._id = _doc.id;
+      return slouch.doc.get('testdb', doc._id);
+    }).then(function (body) {
+      doc._rev = body._rev;
+      doc.priority = 'medium';
+      return slouch.doc.put('testdb', doc);
+    }).then(function () {
+      return slouch.doc.get('testdb', doc._id);
+    }).then(function (body) {
+      doc._rev = body._rev;
+      body.should.eql(doc);
+    });
+  });
+
+  it('should destroy all non-design docs', function () {
+    return slouch.doc.post('testdb', {
+      thing: 'play'
+    }).then(function () {
+      return slouch.doc.post('testdb', {
+        thing: 'write'
+      });
+    }).then(function () {
+      return slouch.doc.post('testdb', {
+        _id: '_design/mydesign',
+        foo: 'bar'
+      });
+    }).then(function () {
+      return slouch.doc.destroyAllNonDesign('testdb');
+    }).then(function () {
+      return slouch.doc.allArray('testdb');
+    }).then(function (body) {
+      body.total_rows.should.eql(1);
+    });
+  });
+
+  it('should throw when conflict', function () {
+    return sporks.shouldThrow(function () {
+      var doc = {
+        thing: 'play'
+      };
+      return slouch.doc.post('testdb', doc).then(function (_doc) {
+        doc._id = _doc.id;
+        doc.priority = 'medium';
+        // Generates conflict as no rev provided
+        return slouch.doc.put('testdb', doc);
+      });
+    });
+  });
+
+});
