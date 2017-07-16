758b240076d8925ebb5948abfa7c88a88f6e470b
Geoff Cox <redgeoff@gmail.com>
2017-07-16 11:21:45 -0700

    test(upsert)

--- test/spec/doc.js
+++ test/spec/doc.js
@@ -36,6 +36,32 @@ describe('doc', function () {
     });
   };
 
+  var defaultGet = null;
+
+  var fakeConflict = function () {
+    return createDocs().then(function () {
+      return slouch.doc.get('testdb', '1');
+    }).then(function (doc) {
+
+      return slouch.doc.createOrUpdate('testdb', {
+        _id: '1',
+        thing: 'dance'
+      }).then(function () {
+
+        defaultGet = slouch.doc.get;
+
+        // Fake conflict
+        slouch.doc.get = function () {
+          return Promise.resolve({
+            _rev: doc._rev
+          });
+        };
+
+      });
+
+    });
+  };
+
   it('should post, put and get doc', function () {
     var doc = {
       thing: 'play'
@@ -160,31 +186,50 @@ describe('doc', function () {
   });
 
   it('should should ignore conflict when creating or updating', function () {
+    return fakeConflict().then(function () {
+      return slouch.doc.createOrUpdateIgnoreConflict('testdb', {
+        _id: '1',
+        thing: 'sing'
+      });
+    });
+  });
 
+  it('should upsert when conflict', function () {
+    var i = 0,
+      defaultCreateOrUpdate = slouch.doc.createOrUpdate;
 
-    return createDocs().then(function () {
-      return slouch.doc.get('testdb', '1').then(function (doc) {
+    // Fake resolution of conflict
+    slouch.doc.createOrUpdate = function () {
+      if (i++ > 3) {
+        // Resolve after a few attempts
+        slouch.doc.get = defaultGet;
+      }
+      return defaultCreateOrUpdate.apply(this, arguments);
+    };
 
-        return slouch.doc.createOrUpdate('testdb', {
+    return fakeConflict().then(function () {
+      return slouch.doc.upsert('testdb', {
+        _id: '1',
+        thing: 'dance'
+      });
+    }).then(function () {
+      return slouch.doc.get('testdb', '1');
+    }).then(function (doc) {
+      doc._id.should.eql('1');
+      doc.thing.should.eql('dance');
+    });
+  });
+
+  it('upsert should fail after max retries', function () {
+    slouch.maxRetries = 3;
+
+    return fakeConflict().then(function () {
+      return sporks.shouldThrow(function () {
+        return slouch.doc.upsert('testdb', {
           _id: '1',
           thing: 'dance'
-        }).then(function () {
-
-          // Fake conflict
-          slouch.doc.get = function () {
-            return Promise.resolve({
-              _rev: doc._rev
-            });
-          };
-
-          return slouch.doc.createOrUpdateIgnoreConflict('testdb', {
-            _id: '1',
-            thing: 'sing'
-          });
-
         });
       });
-
     });
   });
 
