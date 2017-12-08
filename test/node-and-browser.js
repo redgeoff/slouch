0adbd365adecfdc552c5e3f94b1e83fe0736573f
Geoff Cox <redgeoff@gmail.com>
2017-12-08 14:58:04 -0800

    fix(circleci): use port 15984 (#62)
    * fix(circleci): use port 15984
    
    * fix(circleci): use config for port
    
    * fix(jshint)
    
    * fix(circleci): 15984

--- test/node-and-browser.js
+++ test/node-and-browser.js
@@ -4,12 +4,6 @@ var chai = require('chai');
 chai.use(require('chai-as-promised'));
 chai.should();
 
-var config = require('./config.json');
-
-if (process.env.COUCHDB_PORT) {
-  config.couchdb.port = process.env.COUCHDB_PORT;
-}
-
 describe('slouch', function () {
 
   // Sometimes the DB gets a little backed up so we need more time for our tests
